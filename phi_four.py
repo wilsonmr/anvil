5e8f782c1dac1ee07b54c22721d644c914d34a96
Cameron <cameronjohnston53@outlook.com>
2019-10-25 15:11:40 +0100

    added new action and shift methods

--- phi_four.py
+++ phi_four.py
@@ -1,117 +1,91 @@
 from tqdm import tqdm
 import sys
 import time
+from math import ceil
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 import torch
 import torch.optim as optim
+import torch.nn as nn
 
 from norm_flow_pytorch import NormalisingFlow, shifted_kl
-
-class array_mapping(object):
-
-    def __init__(self, array):
-        self.array = array
-        assert array.shape[0]==array.shape[1], 'Lattice should be square'
-        assert array.shape[0]%2==0, 'Lattice should have even number of points per side'
-        self.L = array.shape[0]
-        self.flattened = self.flatten_checkerboard(self.array)
-        self.indices_array()
-        self.nn_indices()
-        self.nn_arrays = np.array([self.nns_1, self.nns_2, self.nns_3, self.nns_4])
-        self.get_nn_val_array()
-
-    @staticmethod
-    def flatten_checkerboard(array):
-        # Takes LxL array and returns flattened 1x2L array in 
-        # checkerboard pattern.
-        a, b = [], []
-        y, x = array.shape[0], array.shape[1]
-        for index, x in np.ndenumerate(array):
-            if (index[0]+index[1])%2 == 0:
-                a.append(x)
-            else:
-                b.append(x)
-        flattened = np.hstack((a,b))
-        return flattened
-
-    def indices_array(self):
-        # Creates array in same shape as original array where
-        # each element gives the index in the flattened array of the
-        # object at that position
-        indices = np.zeros((self.L, self.L))
-        for index, x in np.ndenumerate(self.array):
-            indices[index] = (self.L*index[0] +index[1] - index[1]%2)/2
-            if (index[0]+index[1])%2!=0:
-                indices[index]+=self.L**2 /2
-        self.flat_indices = indices.astype(int)
-
-    def nn_indices(self):
-        # Each array here gives nearest neighbours in the given direction, 1-4.
-        # The object at (i,j) is the index in the flat array of the nearest neighbour
-        # to the object at (i,j) in the input array.
-        self.nns_1 = self.flatten_checkerboard(np.roll(self.flat_indices, -1, axis=1))
-        self.nns_2 = self.flatten_checkerboard(np.roll(self.flat_indices, 1, axis=1))
-        self.nns_3 = self.flatten_checkerboard(np.roll(self.flat_indices, -1, axis=0))
-        self.nns_4 = self.flatten_checkerboard(np.roll(self.flat_indices, 1, axis=0))
-    
-    def get_nn_val_array(self):
-        # Create array. Each row is nns in direction [row]+1.
-        # Each entry is the value of the nearest neighbour in given direction
-        # for entry in flat array in that position.
-        self.nn_vals = np.zeros((4, self.L**2))
-        for k in range(4):
-            for l in range(self.L**2):
-                index = self.nn_arrays[k, l]
-                self.nn_vals[k,l] = self.flattened[index]
-
-    def get_nns_by_flat_index(self, index, directions):
-        assert type(directions)==list, 'Directions parameter must be a list'
-        assert all(d>0 and d<5 and type(d)==int for d in directions), 'All directions must be integers in range 1-4'
-        nns = []
-        for direction in directions:
-            nns.append(self.nn_vals[direction-1, index])
-        return nns
-
-    @staticmethod
-    def split(array, a_left=True):
-        half = int(len(array)/2)
-        if a_left == True:
-            left, right = array[:half], array[half:]
-        else:
-            right, left = array[:half], array[half:]
-        return left, right
 
 random = np.random.RandomState(1234)
 L = 6
 N_UNITS = L**2
 m_sq, l = -4, 6.975
-mu, sigma = 0, 1
 N_BATCH = 2000
-phi_initial_np = (random.normal(mu, sigma, (L, L))).astype(np.float32)
-phi_obj = array_mapping(phi_initial_np)
-phi = phi_obj.flattened
-indices_array = phi_obj.indices_array
-shift = phi_obj.nn_arrays
-phi = torch.from_numpy(phi)
-# A_NP = np.array(np.random.rand(N_UNITS, N_UNITS), dtype=np.float32)
-# A = torch.from_numpy(A_NP)
-# COV_TARGET = A@A.T
-# INV = torch.inverse(COV_TARGET)
 
-def target_distribution_s(phi, m_sq, l, shift):
-    r"""Returns action S(\phi) for a stack of states \phi, shape (N_states, D).
+def get_shift(length: int) -> torch.Tensor:
+    r"""Given a 2D state of size lengthxlength returns a 4x(length^2)
+    tensor where each row gives the 4 nearest neighbours to a flattened state
+    which has been split into (\phi_even, \phi_odd) where even/odd refer to
+    parity of the site
     """
-    action = 0
-    action += (2+0.5*m_sq)*torch.sum(phi**2, dim=1) + l*torch.sum(phi**4, dim=1)
-    for i in range(phi.shape[1]):
-        phi_x = phi[:,i]
-        neighbours_indexes = shift[:,i]
-        action -= 0.5*phi_x*torch.sum(phi[:,neighbours_indexes], dim=1)
-    action = torch.reshape(action, (phi.shape[0],1))
-    return action
+    # define a checkerboard
+    checkerboard = torch.zeros((length, length), dtype=torch.int)
+    # set even sites to 1
+    checkerboard[1::2, 1::2] = 1
+    checkerboard[::2, ::2] = 1
+
+    # make 2d state-like matrix filled with corresponding indices in split-flat state
+    splitind_like_state = torch.zeros((length, length), dtype=torch.int)
+    splitind_like_state[checkerboard.bool()] = torch.arange(
+        int(ceil(length**2/2)),
+        dtype=torch.int,
+    )
+    splitind_like_state[~checkerboard.bool()] = torch.arange(
+        int(ceil(length**2/2)),
+        length**2,
+        dtype=torch.int,
+    )
+
+    flat_checker = checkerboard.flatten()
+    # make split-flat state like object with corresponding indices in flat state
+    out_ind = torch.cat(
+        [torch.where(flat_checker == 1)[0], torch.where(flat_checker == 0)[0]],
+        dim=0,
+    )
+
+    direction_dimension = [
+        (-1, 1),
+        (1, 1),
+        (-1, 0),
+        (1, 0)
+    ]
+    shift = torch.zeros(4, length*length, dtype=torch.long)
+    for i, (direction, dim) in enumerate(direction_dimension):
+        # each shift, roll the 2d state-like indices and then flatten and split
+        shift[i, :] = splitind_like_state.roll(direction, dims=dim).flatten()[out_ind]
+    return shift
+
+
+class PhiFourAction(nn.Module):
+    """Extend the nn.Module class to return the phi^4 action given a state
+    might be possible to jit compile this to make training a bit faster
+    """
+    def __init__(self, length, m_sq, lam):
+        super(PhiFourAction, self).__init__()
+        self.shift = get_shift(length)
+        self.lam = lam
+        self.m_sq = m_sq
+        self.length = length
+
+    def forward(self, phi_state: torch.Tensor) -> torch.Tensor:
+        """Given a stack of states, calculate the action for each state"""
+        action = (
+            (2+0.5*self.m_sq)*phi_state**2 + # phi^2 terms
+            self.lam*phi_state**4 - #phi^4 term
+            0.5*torch.sum(
+                phi_state[:, self.shift]*phi_state.view(-1, 1, self.length**2),
+                dim=1,
+            ) # derivative
+        ).sum(dim=1, keepdim=True) # sum across sites
+        return action
+
+action = PhiFourAction(L, m_sq, l)
 
 def split_transformation(length, a_left=True):
     r"""Given a flattened 2D state, represented by a vector \phi, returns a
@@ -148,7 +122,7 @@ def train(model, epochs, a_left, b_right):
         # gen simple states
         z = torch.randn((N_BATCH, N_UNITS))
         phi = model.inverse_map(z)
-        target = target_distribution_s(phi, m_sq, l, shift)
+        target = action(phi)
         output = model(phi)
 
         model.zero_grad() # get rid of stored gradients
@@ -175,10 +149,8 @@ def sample(model, a_left, b_right, n_large, target_length):
     with torch.no_grad(): # don't want gradients being tracked in sampling stage
         z = torch.randn((n_large, N_UNITS)) # random z configurations
         phi = model.inverse_map(z) # map using trained model to phi
-        # p_tilde = torch.exp(model.forward(phi)) # probabilities of generated phis using trained model
-        log_ptilde = model.forward(phi)
-        # p = torch.exp(-target_distribution_s(phi, m_sq, l, shift)) # probabilities of phis from target pdf
-        S = -target_distribution_s(phi, m_sq, l, shift)
+        log_ptilde = model.forward(phi) # log of probabilities of generated phis using trained model
+        S = action(phi)
         chain_len = 0 # intialise current chain length
         sample_distribution = torch.Tensor(target_length, N_UNITS) # intialise tensor to store samples
         accepted, rejected = 0,0 # track accept/reject statistics
@@ -189,10 +161,8 @@ def sample(model, a_left, b_right, n_large, target_length):
             j = np.random.randint(n_large) # random initial phi^j for update proposal
             while j in used: # make sure we don't pick a phi we have already used
                 j = np.random.randint(n_large)
-            # ratio = (p_tilde[i]/p_tilde[j]) * (p[j]/p[i]) # calculate the ratio in A
-            exponent = log_ptilde[i] + S[j] - log_ptilde[j] - S[i]
+            exponent = log_ptilde[i] - S[j] - log_ptilde[j] + S[i]
             P_accept = np.exp(float(exponent)) # much faster if you tell it to use a float
-            # A = min(1, ratio) # set A
             A = min(1, P_accept) # faster than np.min and torch.min
             u = np.random.uniform() # pick a random u for comparison
             if u <= A:
@@ -208,6 +178,7 @@ def sample(model, a_left, b_right, n_large, target_length):
     return sample_distribution
 
 def main():
+    
     # set seed, hopefully result is reproducible
     torch.manual_seed(0)
     a_left, b_right = split_transformation(L)
