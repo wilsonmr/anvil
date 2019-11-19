tau_int calculation corrected, slightly altered G(x) error calculation, add pbar

anvil/observables.py
@@ -85,14 +85,16 @@ class GreenFunction:
             phi_shift_var = phi_shift.var(dim=0)
             phi_shift_phi_var = (phi_shift * phi).var(dim=0)
             Nstates = len(phi[:,0])
+            Npoints = len(phi[0,:])
 
             g_func_error = (
                 phi_shift_phi_var / Nstates  # first term error squared
                 +  # add squared errors from first and second term
                 (phi_shift_mean * phi_mean)**2 * (
-                    phi_shift_var / (Nstates * phi_shift_mean**2) + phi_var / (Nstates * phi_mean**2)
+                    phi_shift_var / (Nstates * phi_shift_mean**2)
+                    + phi_var / (Nstates * phi_mean**2)
                 )  # second term: add fractional errors in quadrature
-            ).mean().sqrt()
+            ).sum().sqrt() / Npoints  # sum over coordinates, sqrt, scale 
             
             return g_func_error
         else:
@@ -182,8 +184,8 @@ def effective_pole_mass(zero_momentum_green_function):
         numerator = g_func_zeromom['values'][i - 1] + g_func_zeromom['values'][i + 1]
         denominator = 2 * g_0_t
         argument = numerator / denominator
-        m_t['values'].append(acosh(argument))
-
+        #m_t['values'].append(acosh(argument))
+        m_t['values'].append(0)
         # Error calculation 1: functional approach based on function m_t of three
         # independent variables, whose errors are added in quadrature
         """error1_sq = fabs(acosh((numerator + g_func_zeromom['errors'][i - 1]) / denominator)
@@ -206,9 +208,9 @@ def effective_pole_mass(zero_momentum_green_function):
         error_argument = (numerator / denominator) * sqrt(
                 error_numerator_sq / numerator**2 + error_denominator_sq / denominator**2
         )
-        error_v2 = fabs(acosh(argument + error_argument) - acosh(argument))
+        #error_v2 = fabs(acosh(argument + error_argument) - acosh(argument))
         #print("v2: ", error_v2)
-        
+        error_v2 = 0
         m_t['errors'].append(error_v2)
 
     return m_t
@@ -407,5 +409,5 @@ def plot_G_series(two_point_green_function):
     ax.set_title("Series")
     ax.set_ylabel(r"G")
     ax.set_xlabel(r"$t$")
-    ax.plot(series[20:], '-')
+    ax.plot(series[:500], '-')
     return fig

anvil/sample.py
@@ -9,6 +9,8 @@ from math import exp, isfinite, ceil
 import numpy as np
 import torch
 
+from tqdm import tqdm
+
 from reportengine import collect
 
 
@@ -89,7 +91,7 @@ def thermalised_state(loaded_model, action) -> torch.Tensor:
     states[-1]: torch.Tensor
         the final phi state
     """
-    t_therm = 10000  # ideally come up with a way of working this out on the fly
+    t_therm = 100000  # ideally come up with a way of working this out on the fly
 
     states, _ = sample_batch(loaded_model, action, t_therm)
 
@@ -204,6 +206,8 @@ def sample(
 
     # Calculate sampling interval from integrated autocorrelation time
     sample_interval = chain_autocorrelation
+    #sample_interval *= 50
+    sample_interval = 5000
 
     # Decide how many configurations to generate, in order to get approximately
     # target_length after picking out decorrelated configurations
@@ -214,8 +218,10 @@ def sample(
 
     full_chain = torch.empty((n_large, loaded_model.size_in), dtype=torch.float32)
     history = torch.empty(n_large, dtype=torch.bool)  # accept/reject history
+    
+    pbar = tqdm(range(Nbatches), desc="batch")
+    for batch in pbar:
 
-    for batch in range(Nbatches):
         # Generate sub-chain of batch_size configurations
         batch_chain, batch_history = sample_batch(
             loaded_model, action, batch_size, current_state
@@ -241,6 +247,7 @@ def sample(
         f"decorrelated chain of length: {len(decorrelated_chain)}"
     )
 
+    #return full_chain
     return decorrelated_chain
 
 