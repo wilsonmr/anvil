c328b4322c4b34c92d5e870aacb53a6b19d6d7fd
c-j-johnston <cameronjohnston53@outlook.com>
2019-11-22 13:09:48 +0000

    added try/except condition for arccosh ValueError

--- anvil/observables.py
+++ anvil/observables.py
@@ -3,35 +3,32 @@ observables.py
 
 functions for calculating observables on a stack of states.
 
-Functions
----------
-print_plot_observables:
-    can be considered the main function which in turn calculates the rest
-    of the observables defined in this module.}
-
 Notes
 -----
 Check the definitions of functions, most are defined according the the arxiv
 version: https://arxiv.org/pdf/1904.12072.pdf
 
 """
-from math import acosh
+from math import acosh, sqrt, fabs
 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
+from matplotlib.ticker import MaxNLocator
 from scipy.signal import correlate
+import torch
 
-from reportengine.table import table
-from reportengine.figure import figure
+from tqdm import tqdm
+
+from reportengine import collect
 
 
-class GreenFunction:
+class TwoPointFunction:
     def __init__(self, states, geometry):
         self.geometry = geometry
         self.sample = states
 
-    def __call__(self, x_0: int, x_1: int):
+    def __call__(self, x_0: int, x_1: int, sample_size=None):
         r"""Calculates the two point connected green function given a set of
         states G(x) where x = (x_0, x_1) refers to a shift applied to the fields
         \phi
@@ -42,6 +39,9 @@ class GreenFunction:
             shift of dimension 0
         x_1: int
             shift of dimension 1
+        sample_size: int
+            compute the 2pf from a subsample of states, where the indices of the
+            sample_size states are chosen randomly, with replacement
 
         Returns
         -------
@@ -53,30 +53,100 @@ class GreenFunction:
             -1
         )  # make 1d
 
-        g_func = (self.sample[:, shift] * self.sample).mean(
-            dim=0
-        ) - self.sample[  # mean over states
-            :, shift
-        ].mean(
-            dim=0
-        ) * self.sample.mean(
-            dim=0
-        )
-        return g_func.mean()  # integrate over y and divide by volume
+        phi = self.sample
+        phi_shift = self.sample[:, shift]
+
+        if sample_size is not None:
+            Nstates = len(phi[:, 0])
+            sample_indices = np.random.choice(Nstates, sample_size, replace=True)
+            phi = phi[sample_indices, :]
+            phi_shift = phi_shift[sample_indices, :]
+
+        #  Average over stack of states
+        phi_mean = phi.mean(dim=0)
+        phi_shift_mean = phi_shift.mean(dim=0)
+        phi_shift_phi_mean = (phi_shift * phi).mean(dim=0)
+
+        g_func = torch.mean(phi_shift_phi_mean - phi_shift_mean * phi_mean)
+
+        return g_func
 
 
-class VolumeAveraged2pf(GreenFunction):
-    def __call__(self, x_0: int, x_1: int):
+class TwoPointFunctionError:
+    def __init__(self, states, geometry):
+        self.sample = states
+        self.geometry = geometry
+
+    def __call__(self, x_0, x_1):
         """
-        Return torch Tensor of volume-averaged two point functions, i.e.
-        where <\phi(x)> is a mean over points within a single configuration.
-        
+        Calculates the statistical uncertainty in G(x), based on the assumption
+        that the \phi fields are statistically independent, so that the error on
+        the mean is the standard error, i.e. the standard deviation divided by
+        the square root of the number of fields being averaged over.
+
+        Standard errors in <\phi(x)>, <\phi(x+shift)>, <\phi(x+shift)\phi(x)> are
+        propagated to get an error for G(x) using the functional approach.
+        See Hughes & Hase, Measurements and their uncertainties for details.
+
         Parameters
         ----------
         x_0: int
             shift of dimension 0
         x_1: int
             shift of dimension 1
+
+        Returns
+        -------
+        g_func_error: float
+        """
+        shift = self.geometry.get_shift(shifts=((x_0, x_1),), dims=((0, 1),)).view(
+            -1
+        )  # make 1d
+
+        phi = self.sample
+        phi_shift = self.sample[:, shift]
+
+        phi_mean = phi.mean(dim=0)
+        phi_shift_mean = phi_shift.mean(dim=0)
+        phi_shift_phi_mean = (phi_shift * phi).mean(dim=0)
+
+        phi_var = phi.var(dim=0)
+        phi_shift_var = phi_shift.var(dim=0)
+        phi_shift_phi_var = (phi_shift * phi).var(dim=0)
+
+        Nstates = len(phi[:, 0])
+        Npoints = len(phi[0, :])
+
+        g_func_error = (
+            phi_shift_phi_var / Nstates  # first error term squared
+            + (  # add squared errors from first and second term
+                phi_shift_mean * phi_mean
+            )
+            ** 2
+            * (
+                phi_shift_var / (Nstates * phi_shift_mean ** 2)
+                + phi_var / (Nstates * phi_mean ** 2)
+            )  # second error term squared
+        ).sum().sqrt() / sqrt(
+            Npoints
+        )  # sum over coords, sqrt, scale by rootN
+
+        return g_func_error
+
+
+class VolumeAveraged2pf(TwoPointFunction):
+    def __call__(self, x_0: int, x_1: int):
+        """
+        Return torch Tensor of volume-averaged two point functions, i.e.
+        where <\phi(x)> is a mean over points within a single configuration.
+
+        Parameters
+        ----------
+        x_0: int
+            shift of dimension 0
+        x_1: int
+            shift of dimension 1
+
         Returns
         -------
         va_2pf: torch.Tensor
@@ -84,211 +154,264 @@ class VolumeAveraged2pf(GreenFunction):
             for each state in the sample
         """
         shift = self.geometry.get_shift(shifts=((x_0, x_1),), dims=((0, 1),)).view(-1)
-
         va_2pf = (self.sample[:, shift] * self.sample).mean(dim=1) - self.sample.mean(
             dim=1
         ).pow(2)
         return va_2pf
 
 
-def two_point_green_function(sample_training_output, training_geometry):
-    r"""Return instance of GreenFunction which can be used to calculate the
-    two point green function for a given seperation
-    """
-    return GreenFunction(sample_training_output, training_geometry)
+class ZeroMomentum2pf:
+    def __init__(self, training_geometry, two_point_function):
+        self.training_geometry = training_geometry
+        self.two_point_function = two_point_function
+
+    def __call__(self, sample_size=None):
+        r"""Calculate the zero momentum green function as a function of t
+        \tilde{G}(t, 0) which is assumed to be in the first dimension defined as
+
+            \tilde{G}(t, 0) = 1/L \sum_{x_1} G(t, x_1)
+
+        Parameters
+        ----------
+        sample_size: int
+            calculation done based on a subsample of states. See TwoPointFunction
+
+        Returns
+        -------
+        g_func_zeromom: list
+            'values': zero momentum green function as function of t, where t runs from 0 to
+                length - 1
+
+        Notes
+        -----
+        This is \tilde{G}(t, 0) as defined in eq. (23) of
+        https://arxiv.org/pdf/1904.12072.pdf (defined as mean instead of sum over
+        spacial directions) and with momentum explicitly set to zero.
+
+        """
+        g_func_zeromom = []
+        for t in range(self.training_geometry.length):
+            g_tilde_t = 0
+            for x in range(self.training_geometry.length):
+                # not sure if factors are correct here should we account for
+                # forward-backward in green function?
+                g_tilde_t += float(self.two_point_function(t, x, sample_size))
+
+            g_func_zeromom.append(g_tilde_t / self.training_geometry.length)
+        return g_func_zeromom
 
 
-def volume_averaged_2pf(sample_training_output, training_geometry):
-    return VolumeAveraged2pf(sample_training_output, training_geometry)
+class EffectivePoleMass:
+    def __init__(self, zero_momentum_2pf):
+        self.zero_momentum_2pf = zero_momentum_2pf
+
+    def __call__(self, sample_size=None):
+        r"""Calculate the effective pole mass m^eff(t) defined as
+
+            m^eff(t) = arccosh(
+                (\tilde{G}(t-1, 0) + \tilde{G}(t+1, 0)) / (2 * \tilde{G}(t, 0))
+            )
+
+        from t = 1 to t = L-2, where L is the length of lattice side
+
+        Parameters
+        ----------
+        sample_size: int
+            calculation done based on a subsample of states. See TwoPointFunction
+
+        Returns
+        -------
+        m_t: list
+            effective pole mass as a function of t
+
+        Notes
+        -----
+        This is m^eff(t) as defined in eq. (28) of
+        https://arxiv.org/pdf/1904.12072.pdf
+
+        """
+        g_func_zeromom = self.zero_momentum_2pf(sample_size)
+        m_t = []
+        for i, g_0_t in enumerate(g_func_zeromom[1:-1], start=1):
+
+            numerator = g_func_zeromom[i - 1] + g_func_zeromom[i + 1]
+            denominator = 2 * g_0_t
+            argument = numerator / denominator
+            try:
+                m_t.append(acosh(argument))
+            except ValueError:
+                m_t.append(0)
+
+        return m_t
 
 
-def zero_momentum_green_function(training_geometry, two_point_green_function):
-    r"""Calculate the zero momentum green function as a function of t
-    \tilde{G}(t, 0) which is assumed to be in the first dimension defined as
+class Susceptibility:
+    def __init__(self, training_geometry, two_point_function):
+        self.training_geometry = training_geometry
+        self.two_point_function = two_point_function
 
-        \tilde{G}(t, 0) = 1/L \sum_{x_1} G(t, x_1)
+    def __call__(self, sample_size=None):
+        r"""Calculate the susceptibility, which is the sum of two point connected
+        green functions over all seperations
 
-    Returns
-    -------
-    g_func_zeromom: list
-        zero momentum green function as function of t, where t runs from 0 to
-        length - 1
+            \chi = sum_x G(x)
 
-    Notes
-    -----
-    This is \tilde{G}(t, 0) as defined in eq. (23) of
-    https://arxiv.org/pdf/1904.12072.pdf (defined as mean instead of sum over
-    spacial directions) and with momentum explicitly set to zero.
+        Parameters
+        ----------
+        sample_size: int
+            calculation done based on a subsample of states. See TwoPointFunction
 
-    """
-    g_func_zeromom = []
-    for t in range(training_geometry.length):
-        g_tilde_t = 0
-        for x in range(training_geometry.length):
-            # not sure if factors are correct here should we account for
-            # forward-backward in green function?
-            g_tilde_t += float(two_point_green_function(t, x))
-        g_func_zeromom.append(g_tilde_t / training_geometry.length)
-    return g_func_zeromom
+        Returns
+        -------
+        chi: float
+            value for the susceptibility
 
+        Notes
+        -----
+        as defined in eq. (25) of https://arxiv.org/pdf/1904.12072.pdf
 
-def effective_pole_mass(zero_momentum_green_function):
-    r"""Calculate the effective pole mass m^eff(t) defined as
+        """
+        chi = 0
+        for t in range(self.training_geometry.length):
+            for x in range(self.training_geometry.length):
+                chi += float(self.two_point_function(t, x, sample_size))
 
-        m^eff(t) = arccosh(
-            (\tilde{G}(t-1, 0) + \tilde{G}(t+1, 0)) / (2 * \tilde{G}(t, 0))
-        )
-
-    from t = 1 to t = L-2, where L is the length of lattice side
-
-    Returns
-    -------
-    m_t: list
-        effective pole mass as a function of t
-
-    Notes
-    -----
-    This is m^eff(t) as defined in eq. (28) of
-    https://arxiv.org/pdf/1904.12072.pdf
-
-    """
-    g_func_zeromom = zero_momentum_green_function
-    m_t = []
-    for i, g_0_t in enumerate(g_func_zeromom[1:-1], start=1):
-        m_t.append(acosh((g_func_zeromom[i - 1] + g_func_zeromom[i + 1]) / (2 * g_0_t)))
-    return m_t
+        return chi
 
 
-def susceptibility(zero_momentum_green_function):
-    r"""Calculate the susceptibility, which is the sum of two point connected
-    green functions over all seperations
+class IsingEnergy:
+    def __init__(self, two_point_function):
+        self.two_point_function = two_point_function
 
-        \chi = sum_x G(x)
+    def __call__(self, sample_size=None):
+        r"""Ising energy defined as
 
-    Parameters
-    ----------
-    g_func_zeromom: list
-        zero momentum green function as a function of t
+            E = 1/d sum_{\mu} G(\mu)
 
-    Returns
-    -------
-    chi: float
-        value for the susceptibility
+        where \mu is the possible unit shifts for each dimension: (1, 0) and (0, 1)
+        in 2D
 
-    Notes
-    -----
-    as defined in eq. (25) of https://arxiv.org/pdf/1904.12072.pdf
+        Parameters
+        ----------
+        sample_size: int
+            calculation done based on a subsample of states. See TwoPointFunction
 
-    """
-    g_func_zeromom = zero_momentum_green_function
-    # TODO: we can write this in a more efficient and clearer way.
-    partial_sum = [len(g_func_zeromom) * el for el in g_func_zeromom]  # undo the mean
-    return sum(partial_sum)
+        Returns
+        -------
+        E: float
+            value for the Ising energy
+
+        Notes
+        -----
+        as defined in eq. (26) of https://arxiv.org/pdf/1904.12072.pdf
+
+        """
+        E = (
+            self.two_point_function(1, 0, sample_size)
+            + self.two_point_function(0, 1, sample_size)
+        ) / 2
+        return float(E)
 
 
-def ising_energy(two_point_green_function):
-    r"""Ising energy defined as
-
-        E = 1/d sum_{\mu} G(\mu)
-
-    where \mu is the possible unit shifts for each dimension: (1, 0) and (0, 1)
-    in 2D
-
-    Returns
-    -------
-    E: float
-        value for the Ising energy
-
-    Notes
-    -----
-    as defined in eq. (26) of https://arxiv.org/pdf/1904.12072.pdf
-
-    """
-    E = (
-        two_point_green_function(1, 0) + two_point_green_function(0, 1)
-    ) / 2  # am I missing a factor of 2?
-    return float(E)
+##############################################################################
 
 
 def autocorrelation_2pf(training_geometry, volume_averaged_2pf):
     r"""Compute the autocorrelation of the volume-averaged two point function.
-    Autocorrelation is defined by
-    
-        \Gamma(t) = <G(s)G(s+t)> - <G(s)><G(t)>
-    where G(s) is the volume-averaged two point function at Monte Carlo timestep s.
-    """
-    x = t = 0
-    # Should really look at more than one separation
-    # Could go through all x, t and look for largest integrated autocorrelation time,
-    # though this would not be computationally cheap.
 
+    Autocorrelation is defined by
+
+        \Gamma(t) = <G(s)G(s+t)> - <G(s)><G(t)>
+
+    where G(s) is the volume-averaged two point function at Monte Carlo timestep s.
+
+    Integrated autocorrelation is defined by
+
+        \tau = 0.5 + sum_t \Gamma(t)
+
+    Returns
+    -------
+    autocorrelation: numpy.array
+    integrated_autocorrelation: float
+    """
+    x = t = 0  # Should really look at more than one separation
     G_series = volume_averaged_2pf(x, t)
     G_series -= G_series.mean()
     autocorrelation = correlate(
         G_series, G_series, mode="same"
-    )  # converts to numpy array
+    )  # converts in numpy array
     c = np.argmax(autocorrelation)
     autocorrelation = autocorrelation[c:] / autocorrelation[c]
-
-    # This gives the same results, but is much slower
-    """
-    Nstates = len(G_series)
-    autocorr2 = torch.zeros(Nstates)
-    autocorr2[0] = G_series.pow(2).mean() - G_series.mean() ** 2
-    for t in range(1, Nstates):
-        term1 = torch.mean(G_series[:-t] * G_series[t:])
-        term2 = torch.mean(G_series[:-t]) * torch.mean(G_series[t:])
-        autocorr2[t] = term1 - term2
-    autocorr2 = autocorr2 / autocorr2[0]
-    """
 
     integrated_autocorrelation = 0.5 + np.sum(autocorrelation[1:])
 
     return autocorrelation, integrated_autocorrelation
 
 
-@table
-def ising_observables_table(ising_energy, susceptibility, training_output):
-    res = [[ising_energy], [susceptibility]]
-    df = pd.DataFrame(
-        res, columns=[training_output.name], index=["Ising energy", "susceptibility"]
+def bootstrap(observable, sample_size):
+    mean = observable()
+    bootstrap_results = []
+    Nb = 100  # number of bootstrap samples
+    pbar = tqdm(range(Nb), desc="bootstrap sample")
+    for sample in pbar:
+        bootstrap_results.append(observable(sample_size))
+    bootstrap_results = np.array(bootstrap_results)
+
+    error_sq = np.sum((bootstrap_results - mean) ** 2, axis=0) / Nb
+
+    return mean, np.sqrt(error_sq)
+
+
+def two_point_function(sample_training_output, training_geometry):
+    r"""Return instance of TwoPointFunction which can be used to calculate the
+    two point green function for a given seperation
+    """
+    return TwoPointFunction(sample_training_output, training_geometry)
+
+
+def two_point_function_error(sample_training_output, training_geometry):
+    return TwoPointFunctionError(sample_training_output, training_geometry)
+
+
+def volume_averaged_2pf(sample_training_output, training_geometry):
+    return VolumeAveraged2pf(sample_training_output, training_geometry)
+
+
+def zero_momentum_2pf(training_geometry, two_point_function):
+    return ZeroMomentum2pf(training_geometry, two_point_function)
+
+
+############################################################################
+
+
+def zero_momentum_2pf_out(training_geometry, two_point_function, target_length):
+    print("Computing zero-momentum two point function")
+    return bootstrap(
+        ZeroMomentum2pf(training_geometry, two_point_function), target_length // 10
     )
-    return df
 
 
-@figure
-def plot_zero_momentum_green_function(zero_momentum_green_function, training_geometry):
-    fig, ax = plt.subplots()
-    ax.plot(zero_momentum_green_function, "-r", label=f"L = {training_geometry.length}")
-    ax.set_yscale("log")
-    ax.set_ylabel(r"$\hat{G}(0, t)$")
-    ax.set_xlabel("t")
-    ax.set_title("Zero momentum Green function")
-    return fig
+def effective_pole_mass(zero_momentum_2pf, target_length):
+    print("Computing effective pole mass")
+    return bootstrap(EffectivePoleMass(zero_momentum_2pf), target_length // 10)
 
 
-@figure
-def plot_volume_averaged_2pf(volume_averaged_2pf):
-    fig, ax = plt.subplots()
-    ax.set_title("Volume-averaged two point function")
-    ax.set_ylabel(r"$G_V$")
-    ax.set_xlabel(r"$t$")
-    ax.plot(volume_averaged_2pf(0, 0), "-")
-    return fig
-
-
-@figure
-def plot_autocorrelation_2pf(autocorrelation_2pf):
-    autocorrelation, integrated_autocorrelation = autocorrelation_2pf
-    fig, ax = plt.subplots()
-    # ax.set_yscale("log")
-    ax.set_title("Autocorrelation of volume-averaged two point function")
-    ax.set_xlabel("$t$")
-    ax.set_ylabel("$\Gamma_{G_V}(t)$")
-    ax.plot(autocorrelation, "-")
-    x = 0.8 * (1 + len(autocorrelation))
-    y = 0.8
-    ax.text(
-        x, y, r"$\tau_{\rm int} = $ %.3g" % integrated_autocorrelation, fontsize="large"
+def susceptibility(training_geometry, two_point_function, target_length):
+    print("Computing susceptibility")
+    return bootstrap(
+        Susceptibility(training_geometry, two_point_function), target_length // 10
     )
-    return fig
+
+
+def ising_energy(two_point_function, target_length):
+    print("Computing Ising energy")
+    return bootstrap(IsingEnergy(two_point_function), target_length // 10)
+
+
+##############################################################################
+# Currently not used
+# Will need these if we start looping over different namespaces!
+ising_energy_output = collect("ising_energy", ("training_context",))
+susceptibility_output = collect("susceptibility", ("training_contect",))
+zero_momentum_2pf_output = collect("zero_momentum_2pf_out", ("training_context",))
+effective_pole_mass_output = collect("effective_pole_mass", ("training_context",))
