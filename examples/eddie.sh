b63c93c07678ed6ede126650390c72f01913641b
marshrossney <joerosettenstein@gmail.com>
2020-01-16 18:06:36 +0000

    update eddie script

--- examples/eddie.sh
+++ examples/eddie.sh
@@ -7,6 +7,10 @@
 #$ -pe sharedmem 4
 
 source /etc/profile.d/modules.sh
-module load anaconda/5.3.1
-source activate anvil-dev
+module load igmm/apps/miniconda3/4.5.11
+
+source /exports/igmm/software/pkg/el7/apps/miniconda3/4.5.11/etc/profile.d/conda.sh
+conda activate anvil-dev
+
+bash ./train_to_acceptance.sh ./runcards/train.yml ./runcards/short_sample.yml
 
