7c07c7c486d0c7c4526ec9c391518addf8aaf692
marshrossney <joerosettenstein@gmail.com>
2020-04-02 17:58:42 +0100

    add distribution SphericalUniformDist

--- anvil/distributions.py
+++ anvil/distributions.py
@@ -19,11 +19,11 @@ class NormalDist:
 
     Inputs:
     -------
+    lattice_volume: int
+        Number of lattice sites.
     field_dimension: int
         Number of independent field components at each lattice site.
         Default = 1, i.e. a scalar field.
-    lattice_volume: int
-        Number of lattice sites.
     """
 
     def __init__(self, lattice_volume, field_dimension=1):
@@ -155,3 +155,105 @@ def phi_four_action(m_sq, lam, geometry, use_arxiv_version):
     return PhiFourAction(
         m_sq, lam, geometry=geometry, use_arxiv_version=use_arxiv_version
     )
+
+class SphericalUniformDist:
+    """
+    Class which handles the generation of a sample of field configurations
+    following the uniform distribution on a unit sphere.
+
+    Inputs:
+    -------
+    lattice_volume: int
+        Number of lattice sites.
+    field_dimension: int
+        Number of angles parameterising points on the unit sphere at each
+        lattice site. Equal to N-1 where N is the number of components
+        in the Euclidean vector describing a given point.
+        Default = 1, i.e. a two-component vector field defined on the
+        unit circle.
+    """
+
+    def __init__(self, lattice_volume, field_dimension=1):
+        self.lattice_volume = lattice_volume
+        self.field_dimension = field_dimension
+
+        # Number of components per field configuration =
+        # size of output Tensor at dimension 1
+        self.size_out = self.lattice_volume * self.field_dimension
+
+        if self.dimension is 1:
+            self.generator = self.gen_circular
+        elif self.dimension is 2:
+            self.generator = self.gen_spherical
+        else:
+            self.generator = self.gen_hyperspherical
+
+        # Powers of sin(angle) for each angle except the azimuth
+        # in the probability density function
+        self.sin_pow = torch.arange(self.field_dimension - 1, 0, -1).view(1, -1, 1)
+
+    def __call__(self, n_sample):
+        """Return tensor of values drawn from uniform distribution
+        on a unit sphere of dimension field_dimension.
+        
+        Return shape: (n_sample, field_dimension * lattice_volume).
+        """
+        return self.generator(n_sample)
+
+    def gen_circular(self, n_sample) -> torch.Tensor:
+        """Return tensor of values distributed uniformly on the unit 1-sphere.
+        """
+        return torch.rand(n_sample, self.lattice_volume) * 2 * pi
+
+    def gen_spherical(self, n_sample) -> torch.Tensor:
+        r"""Return tensor of values distributed uniformly on the unit 2-sphere.
+        
+        Uses inversion sampling to map random variables x ~ [0, 1] to the 
+        polar angle \theta which has the marginalised density \sin\theta,
+        via the inverse of its cumulative distribution.
+
+                        \theta = \arccos( 1 - 2 x )
+        """
+        samples = torch.cat(
+            (
+                torch.acos(1 - 2 * torch.rand(n_sample, 1, self.lattice_volume)),
+                torch.rand(n_sample, 1, self.lattice_volume) * 2 * pi,
+            ),
+            dim=1,
+        )
+        return sample.view(n_sample, 2 * self.lattice_volume)
+
+    def gen_hyperspherical(self, n_sample) -> torch.Tensor:
+        """Return  distributed uniformly on the unit N-sphere.
+        
+        Uses Marsaglia's algorithm.
+        """
+        rand_normal = torch.randn(n_samples, self.dimension + 1, self.lattice_volume)
+        points = rand_normal / rand_normal.norm(dim=1, keepdim=True)
+
+        # TODO: convert to self.dimension angles. Kind of a faff and not urgent
+        raise NotImplementedError
+
+    def log_density(self, sample: torch.Tensor) -> torch.Tensor:
+        """Return log probability density of a sample generated from
+        the __call__ method above.
+
+        Return shape: (n_sample, 1) where n_sample is the number of
+        field configurations (the first dimension of sample).
+
+        Note that this is equivalent to the surface area element for
+        the (N-1)-sphere expressed in spherical coordinates.
+        """
+        surf_area_element = (
+            self.sin_pow
+            * torch.log(
+                torch.sin(
+                    states.view(-1, self.field_dimension, self.lattice_volume)[
+                        :, :-1, :
+                    ]
+                )
+            )
+        ).sum(
+            dim=1
+        )  # sum over all angles except azimuth
+        return surf_area_element.sum(dim=1, keepdim=True)  # sum over volume
