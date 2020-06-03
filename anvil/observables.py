78acaa982db4c21dca365f189cb2aa1b512758e3
marshrossney <joerosettenstein@gmail.com>
2020-06-03 23:23:29 +0100

    docstrings on observables actions

--- anvil/observables.py
+++ anvil/observables.py
@@ -54,14 +54,23 @@ def optimal_window(integrated, mult=2.0, eps=1e-6):
 
 
 def two_point_correlator(field_ensemble, n_boot):
+    """Bootstrap sample of two point connected correlation functions for the
+    field ensemble."""
     return field_ensemble.boot_two_point_correlator(n_boot)
 
 
 def zero_momentum_correlator(two_point_correlator):
+    """Two point correlator at zero spatial momentum."""
     return 0.5 * (two_point_correlator.mean(axis=0) + two_point_correlator.mean(axis=1))
 
 
 def effective_pole_mass(zero_momentum_correlator):
+    r"""Effective pole mass defined by
+
+        m_p = cosh^{-1}( (\tilde{G}(t-1) + \tilde{G}(t+1)) / (2 * \tilde{G}(t)) )
+
+    where \tilde{G}(t) is the zero momentum correlator defined in this module.
+    """
     inner_indices = np.arange(1, zero_momentum_correlator.shape[0] - 1, dtype=int)
     return np.arccosh(
         (
@@ -73,18 +82,24 @@ def effective_pole_mass(zero_momentum_correlator):
 
 
 def exponential_correlation_length(effective_pole_mass):
+    """Squared exponential correlation length, defined as the inverse of the pole mass."""
     return (1 / effective_pole_mass) ** 2
 
 
 def susceptibility(two_point_correlator):
+    """Susceptibility defined as the first moment of the two point correlator."""
     return two_point_correlator.sum(axis=(0, 1))
 
 
 def ising_energy(two_point_correlator):
+    """Ising energy density, defined as the two point correlator at the minimum
+    lattice spacing."""
     return (two_point_correlator[1, 0] + two_point_correlator[0, 1]) / 2
 
 
 def second_moment_correlation_length(two_point_correlator, susceptibility):
+    """Squared second moment correlation length, defined as the normalised second
+    moment of the two point correlator."""
     L = two_point_correlator.shape[0]
     x = np.concatenate((np.arange(0, L // 2 + 1), np.arange(-L // 2 + 1, 0)))
     x1, x2 = np.meshgrid(x, x)
@@ -97,6 +112,7 @@ def second_moment_correlation_length(two_point_correlator, susceptibility):
 
 
 def low_momentum_correlation_length(two_point_correlator, susceptibility):
+    """A low-momentum estimate for the squared correlation length."""
     L = two_point_correlator.shape[0]
     kernel = np.cos(2 * pi / L * np.arange(L)).reshape(L, 1, 1)
 
@@ -107,10 +123,14 @@ def low_momentum_correlation_length(two_point_correlator, susceptibility):
 
 
 def two_point_correlator_series(field_ensemble):
+    """The volume-averaged two point correlator for the lowest few shifts along
+    a single axes, with the ensemble dimension interpreted as a series."""
     return field_ensemble.two_point_correlator_series
 
 
 def two_point_correlator_autocorr(two_point_correlator_series):
+    """The autocorrelation function calculated for each shift in the two point
+    correlator series."""
     return np.array(
         [
             autocorrelation(two_point_correlator_series[i])
@@ -120,10 +140,17 @@ def two_point_correlator_autocorr(two_point_correlator_series):
 
 
 def two_point_correlator_integrated_autocorr(two_point_correlator_autocorr):
+    r"""The integrated autocorrelation, as a function of summation window size,
+    for the two point correlator series, defined as
+
+        \tau(W) = 1/2 + \sum_{t=1}^W \Gamma(t)
+    """
     return np.cumsum(two_point_correlator_autocorr, axis=-1) - 0.5
 
 
 def two_point_correlator_optimal_window(two_point_correlator_integrated_autocorr):
+    """The optimal value for the summation window for the two point correlator
+    integrated autocorrelation."""
     return optimal_window(two_point_correlator_integrated_autocorr)
 
 
@@ -133,28 +160,39 @@ def two_point_correlator_optimal_window(two_point_correlator_integrated_autocorr
 
 
 def topological_charge_series(field_ensemble):
+    """The topological charge with the ensemble dimension interpreted as a series."""
     return field_ensemble.topological_charge
 
 
 def _topological_charge(topological_charge_series, n_boot):
+    """A bootstrap sample of topological charges, with the ensemble dimension
+    not yet summed over."""
     return bootstrap_sample(topological_charge_series, n_boot)
 
 
 def topological_charge(_topological_charge):
+    """The ensemble-averaged topological charge."""
     return _topological_charge.mean(axis=-1)
 
 
 def topological_susceptibility(_topological_charge, training_geometry):
+    """Topological susceptibility, defined as the second moment of the topological
+    charge divided by the lattice volume."""
     return _topological_charge.var(axis=-1) / training_geometry.length ** 2
 
 
 def topological_charge_autocorr(topological_charge_series):
+    """The autocorrelation function of the topological charge series."""
     return autocorrelation(topological_charge_series)
 
 
 def topological_charge_integrated_autocorr(topological_charge_autocorr):
+    r"""The integrated autocorrelation, as a function of summation window size,
+    for the topological charge series."""
     return np.cumsum(topological_charge_autocorr) - 0.5
 
 
 def topological_charge_optimal_window(topological_charge_integrated_autocorr):
+    """The optimal value for the summation window for the topological charge
+    integrated autocorrelation."""
     return optimal_window(topological_charge_integrated_autocorr)
